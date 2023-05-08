8e3deffafafe6fdebf6331c7e03de009e70d97bc
DivyaUdhay <divyaudhayaukmar.r@gmail.com>
2023-05-08 14:07:48 -0400

    commit after CRUD

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
+import Dashboard from "./Dashboard";
+import Table from "./Table";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./Product";
+import AddUser from "./AddUser";
+import AddProduct from "./AddProduct";
+import ViewUser from "./ViewUser";
+import Viewproduct from "./Viewproduct";
+
+import EditUser from "./EditUser";
+import EditProduct from "./EditProduct";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div id="page-top">
+        <div id="wrapper">
+          <Sidebar />
+          <div id="content-wrapper" className="d-flex flex-column">
+            <div id="content">
+              <Navbar />
+              <div class="container-fluid">
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/table" element={<Table />} />
+                  <Route path="/product" element={<Product />} />
+                  <Route path="/table/adduser" element={<AddUser />} />
+                  <Route path="/product/addproduct" element={<AddProduct />} />
+                  <Route path='/table/view/:id' element={<ViewUser/>}/>
+                  <Route path='/product/view/:id' element={<Viewproduct/>}/>
+                  <Route path="/table/edit/:id" element={<EditUser/>}/>
+                  <Route path="/product/edit/:id" element={<EditProduct/>}/>
+                </Routes>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
